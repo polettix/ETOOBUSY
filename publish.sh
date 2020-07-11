adc6eab036d08b93117d5082c966a8d07fbffc30
Flavio Poletti <flavio@polettix.it>
2020-07-11 13:44:33 +0200

    Be more lazy with publishing...

--- publish.sh
+++ publish.sh
@@ -3,25 +3,81 @@
 set -eu
 : ${BLOG_PRIVATE_REPO:="private"}
 
-if [ $# -eq 0 ] ; then
-   printf >&2 '%s\n' "$0 <date-tag> <msg> <path(s)>"
-   exit 1
-fi
+info() { printf >&2 '%s\n' "$*"; }
+die() { info "$*"; exit 1; }
 
-tag="$1"
-commit_message="$2"
-shift 2
+publish() {
+   local path=''
+   local date=''
+   local title=''
+   local file
+   for file in "$@" ; do
+      case "$file" in
+         (_posts/*.md)
+            [ -z "$path" ] || die "two posts found, please one at a time!"
+            local fm="$(sed -ne '2,/^---/{/^---/q;p}' "$file")"
+            date="$(printf '%s' "$fm" \
+               | teepee -y - -T '[%= (split m{\s+}mxs, V("date"))[0] %]')"
+            title="$(printf '%s' "$fm" | teepee -y - -v title)"
+            path="$file"
+            ;;
+      esac
+   done
 
-printf >&2 'tag <%s>\ncommit message <%s>\npath(s) <%s>\nOK? (y|*) ' \
-   "$tag" "$commit_message" "$*"
+   
+   [ -n "$date" ]  || die "no date from <$*>"
+   [ -n "$title" ] || die "no title from <$*>"
 
-read x
+   printf >&2 'date <%s>\ntitle <%s>\npath(s) <%s>\nOK? (y|s|*) ' \
+      "$date" "$title" "$*"
 
-if [ "x$x" = 'xy' ] ; then
-git add "$@"
-git commit -m "$commit_message"
-git tag "$tag"
-git push "$BLOG_PRIVATE_REPO" devel "$tag"
+   read x
+   case "$x" in
+      (y|Y)
+         return 0
+         git add "$@"
+         git commit -m "New post: $title"
+         git tag "$date"
+         git push "$BLOG_PRIVATE_REPO" devel "$date"
+         ;;
+      (s|S)
+         info 'skipping as requested'
+         ;;
+      (*)
+         die 'bailing out'
+         ;;
+   esac
+
+   return 0
+}
+
+
+[ $# -ne 0 ] || die "$0 <path(s)>"
+
+[ "$1" != '--all' ] \
+   || set -- $(git status --short --branch | sed -e '/^#/d;s/^...//')
+
+n_posts=0
+n_other=0
+for file in "$@" ; do
+   case "$file" in
+      (_posts/*md)
+         n_posts=$((n_posts + 1))
+         ;;
+      (*)
+         n_other=$((n_other + 1))
+         ;;
+   esac
+done
+
+if [ $n_posts -eq 0 ] ; then
+   die "no post in list <$*>"
+elif [ $n_posts -eq 1 ] ; then
+   publish "$@"
+elif [ $n_other -eq 0 ] ; then
+   for file in "$@" ; do
+      publish "$file"
+   done
 else
-   printf >&2 'not an <y>, bailing out\n'
+   die "either one post with ancillaries, or multiple posts <$*>"
 fi
