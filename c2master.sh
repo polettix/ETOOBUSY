b8fb594486eb86a462e8876e31b28cabfec1c7c9
Flavio Poletti <flavio@polettix.it>
2020-10-04 17:39:58 +0200

    Make c2master.sh a little more robust

--- c2master.sh
+++ c2master.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -eux
 [ $# -eq 0 ] && ref='devel' || ref="$1"
+initial_branch="$(git rev-parse --abbrev-ref HEAD)"
 git checkout master
 git pull
 git merge --ff-only "$ref"
 git push
-git checkout devel
-git push
+git checkout "$initial_branch"
+[ "$initial_branch" != 'devel' ] || git push
