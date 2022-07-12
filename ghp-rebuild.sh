c09b4ba59e71190bbda19ef76730f3efbafa8feb
Flavio Poletti <flavio@polettix.it>
2022-07-12 09:06:26 +0200

    Add some docs for future use

--- ghp-rebuild.sh
+++ ghp-rebuild.sh
@@ -5,14 +5,33 @@ md="$(dirname "$me")"
 
 # Get credentials from the environment or from a file. Yes, this can be
 # improved!
+#
+# NOTE NOTE NOTE
+#
+# - use a Personal Access Token with scope "repo" at
+#   https://github.com/settings/tokens
+#
+# - NO, "repo_public" WILL NOT cut it, full "repo" is needed
+#
+# - just the token, no username or anything in the file or env variable
+#
+# Example:
+#
+#     GHP_CREDENTIALS=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
+#
+# More: https://docs.github.com/en/rest/pages#request-a-github-pages-build
+#
 credentials="${GHP_CREDENTIALS:-"$(cat ~/.github/ghp-rebuild)"}"
+
+
+
 fqrepo="${GHP_FQREPO:-"$(
       teepee -y "$md/_config.yml" \
          -T '[%= (V("origin") =~ m{github.com/([-\w]+/[-\w]+)}mxs)[0] %]'
    )"}"
 
-curl \
-   -u "$credentials" \
+$PRE curl $POST \
    -X POST \
-   -H "Accept: application/vnd.github.v3+json" \
+   -H "Accept: application/vnd.github+json" \
+   -H "Authorization: token $credentials" \
    "https://api.github.com/repos/$fqrepo/pages/builds"
